a551dc6bdabaf6743808bdea0be3f3313940c2ba
Neschadin Oleksandr <73835274+Neschadin@users.noreply.github.com>
2023-07-13 18:41:11 +0300

    small fixs;

--- components/HeaderNavMenu.tsx
+++ components/HeaderNavMenu.tsx
@@ -5,13 +5,13 @@ export const NavMenu = () => (
     <Link className="hover:text-black" href="/">
       Головна
     </Link>
-    <Link className="hover:text-black" href={"/lenovo-warranty"}>
+    <Link className="hover:text-black" href="/lenovo-warranty">
       Гарантія Lenovo
     </Link>
-    <Link className="hover:text-black" href={"/contacts"}>
+    <Link className="hover:text-black" href="/contacts">
       Контакти
     </Link>
-    <Link className="hover:text-black" href={"/about-us"}>
+    <Link className="hover:text-black" href="/about-us">
       Про Нас
     </Link>
   </nav>
